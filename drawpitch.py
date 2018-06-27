b4079158dcf83369bc412c1d7f9ba87c724c17fb
znstrider <34557509+znstrider@users.noreply.github.com>
2018-06-27 11:50:15 +0200

    Update drawpitch.py

--- drawpitch.py
+++ drawpitch.py
@@ -2,10 +2,8 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 from math import pi
 import numpy as np
-import seaborn as sns
-sns.set_style('dark')
 
-def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
+def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34], measure = 'metres',\
                 linecolor = '#525252', facecolor = '#f0f0f0', arcs = True, \
                 lw = 1.5, x_offset = [4,4], y_offset = [4,4], style_id = None,
                 grass_cutting = False):
@@ -13,8 +11,13 @@ def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
     -----
     Draws a horizontal pitch on an axes object with width 105m and height 68m
     -----
-    If you are using StatsBomb Data with a 120x80yard pitch (or any other pitchsize), use:
-    hspan = [0, 120], vspan = [0, 80]
+    If you are using StatsBomb Data with a 120x80yard pitch, use:
+    measure == 'SBData'
+    -----
+    If you are using any other pitch size, set measure to yards or metres
+    for correct pitch markings and
+    hspan = [left, right] // eg. for SBData this is: hspan = [0, 120]
+    vspan = [bottom, top] //
     to adjust the plot to your needs.
     -----
     Choose a Style with
@@ -27,12 +30,23 @@ def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
     or also to just draw half the pitch
     -----
     grass_cutting = n to divide the pitch into n-1 vertical stripes from box to box
-    (works best for eneven n)
+    (works best for even n)
+    grass_cutting = True sets n to 14
     '''
+
+
+    if measure == 'metres':
+        measures = [3.66, 5.5, 9.12, 16.5, 40.24]
+    elif measure == 'yards':
+        measures = [4, 6, 10, 18, 42]
+    elif (measure == 'SBData')|(measure == 'StatsBomb'):
+        measures = [4, 6, 10, 18, 42]
+        hspan = [0, 120]
+        vspan = [0, 80]
+
     hmid = (hspan[1]+hspan[0])/2
     vmid = (vspan[1]+vspan[0])/2
     ax.axis([hspan[0]-x_offset[0],hspan[1]+x_offset[1],vspan[0]-y_offset[0],vspan[1]+y_offset[1]])
-
 
     if style_id:
         style = {1: ["#525252", "#f0f0f0"],
@@ -50,22 +64,22 @@ def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
             raise ValueError('style_id needs to be in range(1, '+str(len(style)+1)+')')
     ax.set_facecolor(facecolor)
 
-    mid_circle = plt.Circle((hmid, vmid), radius=9.15, fc='None', color = linecolor, lw = lw )
+    mid_circle = plt.Circle((hmid, vmid), radius=measures[2], fc='None', color = linecolor, lw = lw )
     ax.add_patch(mid_circle)
 
     mid_point = plt.Circle((hmid, vmid), radius=0.4, fc='white', color = linecolor, lw = lw)
     ax.add_patch(mid_point)
 
-    fiveyard1 = plt.Rectangle((hspan[0], vmid-9.12), 5.5, 18.24, fc='None', color = linecolor, lw = lw)
+    fiveyard1 = plt.Rectangle((hspan[0], vmid-measures[2]), measures[1], measures[2]*2, fc='None', color = linecolor, lw = lw)
     ax.add_patch(fiveyard1)
 
-    fiveyard2 = plt.Rectangle((hspan[1]-5.5, vmid-9.12), 5.5, 18.24, fc='None', color = linecolor, lw = lw)
+    fiveyard2 = plt.Rectangle((hspan[1]-measures[1], vmid-measures[2]), measures[1], measures[2]*2, fc='None', color = linecolor, lw = lw)
     ax.add_patch(fiveyard2)
 
-    box1 = plt.Rectangle((hspan[0], vmid-20.12), 16.5, 40.12, fc='None', color = linecolor, lw = lw)
+    box1 = plt.Rectangle((hspan[0], vmid-measures[4]/2), measures[3], measures[4], fc='None', color = linecolor, lw = lw)
     ax.add_patch(box1)
 
-    box2 = plt.Rectangle((hspan[1]-16.5, vmid-20.12), 16.5, 40.12, fc='None', color = linecolor, lw = lw)
+    box2 = plt.Rectangle((hspan[1]-measures[3], vmid-measures[4]/2), measures[3], measures[4], fc='None', color = linecolor, lw = lw)
     ax.add_patch(box2)
 
     midline = plt.Line2D((hmid, hmid), (vspan[0], vspan[1]), c = linecolor, lw = lw)
@@ -83,29 +97,31 @@ def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
     bottomfieldline = plt.Line2D((hspan[0],hspan[1]),(vspan[0],vspan[0]), c = linecolor, lw = lw)
     ax.add_line(bottomfieldline)
 
-    goal1 = plt.Line2D((hspan[0],hspan[0]),(vmid+3.66,vmid-3.66), c=linecolor, lw = 3*lw)
+    goal1 = plt.Line2D((hspan[0],hspan[0]),(vmid+measures[0],vmid-measures[0]), c=linecolor, lw = 3*lw)
     ax.add_line(goal1)
 
-    goal2 = plt.Line2D((hspan[1],hspan[1]),(vmid+3.66,vmid-3.66), c = linecolor, lw = 3*lw)
+    goal2 = plt.Line2D((hspan[1],hspan[1]),(vmid+measures[0],vmid-measures[0]), c = linecolor, lw = 3*lw)
     ax.add_line(goal2)
 
-    penalty1 = plt.Circle((hspan[0]+11, vmid), radius = 0.4, fc = linecolor, color=linecolor)
+    penalty1 = plt.Circle((hspan[0]+2*measures[1], vmid), radius = 0.4, fc = linecolor, color=linecolor)
     ax.add_patch(penalty1)
 
-    penalty2 = plt.Circle((hspan[1]-11, vmid), radius = 0.4, fc = linecolor, color=linecolor)
+    penalty2 = plt.Circle((hspan[1]-2*measures[1], vmid), radius = 0.4, fc = linecolor, color=linecolor)
     ax.add_patch(penalty2)
 
     if arcs:
-        a = np.arccos((16.5-11)/9.12)
-        arc1 = patches.Arc(xy = (hspan[0]+11, vmid), width = 2*9.12, height = 2*9.12,\
+        a = np.arccos((measures[3]-11)/measures[2])
+        arc1 = patches.Arc(xy = (hspan[0]+11, vmid), width = 2*measures[2], height = 2*measures[2],\
                                 theta1 = (2*pi - a)*180/pi, theta2 = a*180/pi, color = linecolor, lw = lw)
         ax.add_patch(arc1)
-        arc2 = patches.Arc(xy = (hspan[1]-11, vmid), width = 2*9.12, height = 2*9.12,\
+        arc2 = patches.Arc(xy = (hspan[1]-11, vmid), width = 2*measures[2], height = 2*measures[2],\
                                 theta1 = (pi - a)*180/pi, theta2 = (pi + a)*180/pi, color = linecolor, lw = lw)
         ax.add_patch(arc2)
 
     if grass_cutting:
-        xmid = [hspan[0] + 16.5, hspan[1] - 16.5]
+        if grass_cutting == True:
+            grass_cutting = 14
+        xmid = [hspan[0] + measures[3], hspan[1] - measures[3]]
         hbins = np.linspace(xmid[0], xmid[1], grass_cutting)
 
         for i in range(len(hbins)):
@@ -115,13 +131,13 @@ def drawpitch(ax, hspan = [-52.5, 52.5], vspan = [-34, 34],\
                                      vspan[0] - vspan[1],
                                      color = linecolor, lw = 0, alpha = 0.04)
                 ax.add_patch(rect)
-        rect = plt.Rectangle((hspan[0]+5.5, vspan[1]),
-                                     5.5,
+        rect = plt.Rectangle((hspan[0]+measures[1], vspan[1]),
+                                     measures[1],
                                      vspan[0] - vspan[1],
                                      color = linecolor, lw = 0, alpha = 0.04)
         ax.add_patch(rect)
-        rect = plt.Rectangle((hspan[1]-11, vspan[1]),
-                                     5.5,
+        rect = plt.Rectangle((hspan[1]-measures[1], vspan[1]),
+                                     -measures[1],
                                      vspan[0] - vspan[1],
                                      color = linecolor, lw = 0, alpha = 0.04)
         ax.add_patch(rect)
